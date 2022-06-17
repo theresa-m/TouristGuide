d79383f8e16379d6b64989ca7756ede38db41315
Theresa Mammarella <mammarellatheresa8@gmail.com>
2022-06-17 13:28:43 -0400

    Log4j config and more attractions

--- src/main/java/tourguide/KrakowTouristGuide.java
+++ src/main/java/tourguide/KrakowTouristGuide.java
@@ -9,8 +9,9 @@ import java.util.Random;
 
 public class KrakowTouristGuide {
     static final Double DEVOXXPOLANDRADIUS = 0.0;
+    static final Double WALKRADIUS = 2.5;
     static List<Attraction> cityAttractions = new LinkedList<>();
-    //static Logger log = LogManager.getLogger(KrakowTouristGuide.class.getName());
+    static Logger log = LogManager.getLogger(KrakowTouristGuide.class.getName());
 
     /* Attraction name, description, distance from Devoxx Poland 2022 */
     record Attraction(String name, String description, Double distance) {
@@ -21,6 +22,8 @@ public class KrakowTouristGuide {
         private String distanceMessage() {
             if (distance == DEVOXXPOLANDRADIUS) {
                 return "This attraction is at Devoxx Poland!";
+            } else if (distance < WALKRADIUS){
+                return "A walkable " + distance.toString() + " kms from Devoxx Poland 2022";
             } else {
                 return distance.toString() + " kms from Devoxx Poland 2022";
             }
@@ -28,24 +31,43 @@ public class KrakowTouristGuide {
     }
 
     public static void main(String[] args) {
-        try {
-            cityAttractions.add(new Attraction("Sonatype Booth", "Come learn about Sonatype at Devoxx Poland!", 0.0));
-            cityAttractions.add(new Attraction("National Museum",
-                    "3 permanent galleries displaying artworks & historical artifacts from Poland & elsewhere in Europe.",
-                    1.9));
-            cityAttractions.add(new Attraction("Kraków Eye",
-                    "Ferris wheel with a view",
-                    0.35));
+        Random r = new Random();
+        initializeAttractionsList();
+        printIntro();
 
+        while (true) {
+            try {
+                /* wait for enter key press to continue */
+                System.in.read();
 
-            Random r = new Random();
-
-            System.out.println("What should you see while in Kraków, Poland?\n");
-
-            int i = r.nextInt(cityAttractions.size());
-            System.out.println(cityAttractions.get(i).print());
-        } catch(Throwable t) {
-            t.printStackTrace(System.err);
+                /* show user a randomly chosen attraction */
+                Attraction attraction = cityAttractions.get(r.nextInt(cityAttractions.size()));
+                log.info(attraction.name());
+                System.out.println(attraction.print());
+            } catch (Throwable t) {
+                log.error(t.getMessage());
+                t.printStackTrace(System.err);
+            }
         }
+    }
+
+    private static void initializeAttractionsList() {
+        cityAttractions.add(new Attraction("Sonatype Booth", "Come learn about Sonatype at Devoxx Poland!", 0.0));
+        cityAttractions.add(new Attraction("National Museum",
+                "3 permanent galleries displaying artworks & historical artifacts from Poland & elsewhere in Europe.",
+                1.9));
+        cityAttractions.add(new Attraction("Kraków Eye",
+                "Ferris wheel with a view",
+                0.35));
+        cityAttractions.add(new Attraction("St Mary's Basilica",
+                "A brick gothic church bult in the 14th century",
+                2.1));
+        cityAttractions.add(new Attraction("Wawel Royal Castle",
+                "The castle, being one of the largest in Poland, represents nearly all European architectural styles of medieval, renaissance and baroque periods.",
+                1.3));
+    }
+
+    private static void printIntro() {
+        System.out.println("What should you see while in Kraków, Poland?");
     }
 }
