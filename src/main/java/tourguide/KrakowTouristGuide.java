f446b00eaaf2de36410dafb4f5629ec99e79df65
Theresa Mammarella <mammarellatheresa8@gmail.com>
2022-08-15 14:16:06 -0400

    Import ArrayList

--- src/main/java/tourguide/KrakowTouristGuide.java
+++ src/main/java/tourguide/KrakowTouristGuide.java
@@ -3,6 +3,7 @@ package tourguide;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
